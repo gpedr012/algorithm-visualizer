bee0bbf66779bd587069beab40e2cbdc1edfe3f6
Gessler Pedrol <gpedr012@fiu.edu>
2021-06-19 01:58:37 -0400

    Initial Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
+import MainContent from "./components/UI/MainContent/MainContent";
+import Sidebar from "./components/UI/Sidebar/Sidebar";
 
 function App() {
-  return (
-<div>App</div>
-  );
+    return (
+        <div className={classes.content}>
+            <Sidebar/>
+            <MainContent/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/UI/MainContent/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./MainContent.module.css"
+
+const MainContent = () => {
+    return (
+        <div>Main Content</div>
+    )
+}
+
+export default MainContent;
