8ab47411fe81ba5e88e7e145c86c7d51e7e7bc3d
Gessler Pedrol <gpedr012@fiu.edu>
2021-06-27 02:34:59 -0400

    Sidebar functionality finished. Opens and closes, although will need media queries.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "./Sidebar.module.css";
 
 const Sidebar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleSidebarBtn = () => {
+        setIsOpen(oldV => !oldV);
+
+    }
+
+    let buttonClass = isOpen ?  `${classes['button-icon']} ${classes['button-icon__close']}` : classes['button-icon'];
+    let sideBarClass = isOpen ? `${classes.sidebar} ${classes['sidebar__open']}` : classes.sidebar;
+
     return (
-        <div className={classes.sidebar}>
-            <nav>
-                <ul>
-                    <h3>Sorting Algorithms</h3>
-                    <li>Bubble Sort</li>
-                    <li>Selection Sort</li>
-                    <li>Insertion Sort</li>
-                </ul>
-            </nav>
-        </div>
+        <React.Fragment>
+            <div onClick={handleSidebarBtn} className={classes.button}>
+                <div className={buttonClass}>
+                    >
+                </div>
+            </div>
+            <div className={sideBarClass}>
+                <nav>
+                    <ul>
+                        <h3>Sorting Algorithms</h3>
+                        <li>Bubble Sort</li>
+                        <li>Selection Sort</li>
+                        <li>Insertion Sort</li>
+                    </ul>
+                </nav>
+            </div>
+        </React.Fragment>
     );
 }
 
